acfb6c95f5bd908d07cb7ef45ed8c13550195cab
Chaitanya Pal <palchaitanya59@gmail.com>
2026-09-25 21:33:28 +0530

    Add context API project

---
+++ 20-context-api/src/components/Nav2.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Nav2 = (props) => {
+
+
+
+
+  return (
+    <div className='nav2'>
+        <h4>Home</h4>
+        <h4>About</h4>
+        <h4>Contact</h4>
+        <h4>{data}</h4>
+        {props.theme}
+
+    </div>
+  )
+}
+
+export default Nav2
---
+++ 20-context-api/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav2 from './Nav2'
+
+
+const Navbar = (props) => {
+    
+
+  return (
+    <div className='nav'>
+        <h2>{data}</h2>
+        <Nav2 theme={props.theme} />
+
+    </div>
+  )
+}
+
+export default Navbar
